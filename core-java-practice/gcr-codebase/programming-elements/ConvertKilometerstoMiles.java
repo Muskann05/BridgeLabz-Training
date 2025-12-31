60a8d3fee2a873328fcda8548696b7df8ff318b2
Muskann05 <mg3494878@gmail.com>
2025-12-31 11:50:18 +0530

    [deleting : some file]

--- core-java-practice/gcr-codebase/programming-elements/ConvertKilometerstoMiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int Kilometers=sc.nextInt();
-        // calculate miles
-        System.out.println(Kilometers*0.621371);
-    }
-}
--- core-java-practice/gcr-codebase/programming-elements/distancekilometerstomiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double km = sc.nextDouble();
-        double mile = km * 0.621371;
-        System.out.println(mile);
-    }
-}
