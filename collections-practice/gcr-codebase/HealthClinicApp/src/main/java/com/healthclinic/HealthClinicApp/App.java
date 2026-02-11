3b1cc8cc531b280c6ffdc0fa33d61bf883848ca4
Muskann05 <mg3494878@gmail.com>
2026-02-11 21:57:30 +0530

    [Muskan] Add.HealthClinicApp

---
+++ collections-practice/gcr-codebase/HealthClinicApp/src/main/java/com/healthclinic/HealthClinicApp/App.java
@@ -0,0 +1,13 @@
+package com.healthclinic.HealthClinicApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
