050c59142b46b446fd884fc788f3f4cb227c8bfe
Muskann05 <mg3494878@gmail.com>
2026-04-01 05:40:48 +0530

    [muskan] update

--- collections-practice/gcr-codebase/generics/resume-screening-system/ProductManager.java
+++
@@ -1,9 +0,0 @@
-public class ProductManager extends JobRole {
-    public ProductManager() {
-        super("Product Manager");
-    }
-    @Override
-    public String getRequiredSkills() {
-        return "Product Strategy, Communication, Analytics, Leadership";
-    }
-}
